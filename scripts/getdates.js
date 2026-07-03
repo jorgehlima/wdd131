d3b61978a5fb44b7e01eb3aa66410920c65d1ac8
jorgehlima <jlima5@byupathway.edu>
2026-07-03 13:05:18 -0400

    commit upds

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,6 @@
-// 2026
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+// Dynamically set the current year in the footer copyright notice
+document.getElementById('currentyear').textContent = new Date().getFullYear();
 
-//Last modified date
-document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
+// Dynamically display the last modified date of this document
+document.getElementById('lastModified').textContent =
+    `Last Modification: ${document.lastModified}`;
