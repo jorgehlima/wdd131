deac8c7b1267a2fc90d412a32304ba7aab1c5997
jorgehlima <jlima5@byupathway.edu>
2026-07-03 12:42:28 -0400

    upd index and getdates

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// 2026
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//Last modified date
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
