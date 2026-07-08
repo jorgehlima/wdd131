520e9b23ac97dec3eca401a5d68c7abdd678232a
jorgehlima <jlima5@byupathway.edu>
2026-07-08 19:28:09 -0400

    commit upd temples js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,4 +1,19 @@
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+const year = document.querySelector("#currentyear");
 
-document.getElementById("lastModified").textContent =
-    `Last Modification: ${document.lastModified}`;
+const modified = document.querySelector("#lastModified");
+
+year.textContent = new Date().getFullYear();
+
+modified.textContent = `Last Modified: ${document.lastModified}`;
+
+const hamButton = document.querySelector("#menu");
+
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("show");
+
+    hamButton.classList.toggle("open");
+
+});
